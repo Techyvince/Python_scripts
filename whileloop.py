218e76d8e4038723ebd2ea4b7962691be1880e11
Techyvince <ifezueprince@gmail.com>
2022-04-03 19:26:24 -0400

    ..

---
+++ whileloop.py
@@ -0,0 +1,6 @@
+# While loop is a structure that allows us to loop through and excecute a block of code multiple times until a certain condition is meet
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 2
+print("Done with loop")
