fbd8b1aa634fa004e709f1cbddae873e4b50293a
Techyvince <ifezueprince@gmail.com>
2022-04-04 12:33:09 -0400

    ..

---
+++ forloop.py
@@ -0,0 +1,12 @@
+#we can use for loop to loop through array
+
+friends = ["vin", "Chris", "Ben"]
+
+for index in range(5):  
+    if index == 2:
+        print("first Iteration")
+    else:
+        print("Not first")
+
+for letter in "Good Time":
+    print(letter)
